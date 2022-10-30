fb0b29764d6b4a43411e1b5d262020c53591afbd
leiwei <quic_leiwei@quicinc.com>
2022-10-30 21:46:41 +0800

    qca-ssdk: remove unused pppoe hsl codes
    reduce 7k size for 64bit build
    
    Change-Id: I1dc8469184f4f2176a757fc1ef1590bfcc3fca26
    Signed-off-by: leiwei <quic_leiwei@quicinc.com>

--- include/hsl/hppe/hppe_pppoe.h
+++ include/hsl/hppe/hppe_pppoe.h
@@ -1,15 +1,18 @@
 /*
  * Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
- * Permission to use, copy, modify, and/or distribute this software for
- * any purpose with or without fee is hereby granted, provided that the
- * above copyright notice and this permission notice appear in all copies.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
- * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 
@@ -60,6 +63,7 @@ hppe_pppoe_session_ext1_set(
 		a_uint32_t index,
 		union pppoe_session_ext1_u *value);
 
+#if 0
 sw_error_t
 hppe_pppoe_session_session_id_get(
 		a_uint32_t dev_id,
@@ -167,7 +171,7 @@ hppe_pppoe_session_ext1_smac_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-
+#endif
 
 #endif
 

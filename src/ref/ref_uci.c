cb26b0b22f81a5a03ef779baed4f276857be387f
Luo Jie <quic_luoj@quicinc.com>
2023-02-03 17:58:36 +0800

    qca-ssdk: Add host entry uci field of MPPE
    Change-Id: Icf2f1318f858f94d2720e5e4e8115aab04b93b38
    Signed-off-by: Luo Jie <quic_luoj@quicinc.com>

--- src/ref/ref_uci.c
+++ src/ref/ref_uci.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2013, 2015, 2017-2019, 2021, The Linux Foundation. All rights reserved.
  *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -9967,6 +9967,13 @@ static const char *flow_host[] = {
 	"bridge_cvlan_format",
 	"wifi_qos_en",
 	"wifi_qos",
+#endif
+#if defined(MPPE)
+	"qos_type",
+	"bridge_nexthop_valid",
+	"bridge_nexthop",
+	"policer_valid",
+	"policer_index",
 #endif
 	"ipentry_id",
 	"entry_flags",

a5b49d7519ddc938e959a871a4bf7a0c5da477eb
zhongjia <quic_zhongjia@quicinc.com>
2024-01-23 19:13:13 +0800

    [qca-ssdk]: fix swconfig issue for APPE and MPPE
    Change-Id: I20189a4c48ed545ed8c36c999e73eb8fea16ff38
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>

--- src/ref/ref_misc.c
+++ src/ref/ref_misc.c
@@ -1,15 +1,18 @@
 /*
  * Copyright (c) 2012, 2017, The Linux Foundation. All rights reserved.
- * Permission to use, copy, modify, and/or distribute this software for
- * any purpose with or without fee is hereby granted, provided that the
- * above copyright notice and this permission notice appear in all copies.
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
- * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include "sw.h"
@@ -61,9 +64,10 @@ qca_ar8327_sw_set_max_frame_size(struct switch_dev *dev,
 	a_uint32_t ret;
 	a_uint32_t port_id = SSDK_PHYSICAL_PORT0;
 
-	if (priv->version == QCA_VER_HPPE)
+	if (priv->version == QCA_VER_HPPE || priv->version == QCA_VER_APPE
+		|| priv->version == QCA_VER_MRPPE)
 	{
-		for(port_id = SSDK_PHYSICAL_PORT1; port_id <= SSDK_PHYSICAL_PORT6;
+		for(port_id = SSDK_PHYSICAL_PORT1; port_id < priv->ports;
 			port_id++)
 		{
 			ret = fal_port_max_frame_size_set(priv->device_id,
@@ -114,7 +118,8 @@ qca_ar8327_sw_get_max_frame_size(struct switch_dev *dev,
 	a_uint32_t size = 0;
 	a_uint32_t ret = 0;
 
-	if (priv->version == QCA_VER_HPPE)
+	if (priv->version == QCA_VER_HPPE || priv->version == QCA_VER_APPE
+		|| priv->version == QCA_VER_MRPPE)
 	{
 		ret = fal_port_max_frame_size_get(priv->device_id,
 			SSDK_PHYSICAL_PORT1, &size);

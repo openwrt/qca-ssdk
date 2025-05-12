openwrt/qca-ssdk

commit 90cbee1f39d60919781cbeed30c0e39cb68897af

hsl: phy: sfp: work around constified phydev->drv
Linux has constified phydev->drv so we cannot pass it to
phy_driver_unregister directly since v6.9.

So, work around it by looking up the device driver and then getting
the PHY driver structure from it like PHY core does.

Signed-off-by: Robert Marko <robimarko@gmail.com>

src/hsl/phy/sfp_phy.c
@@ -307,6 +307,10 @@ int sfp_phy_device_setup(a_uint32_t dev_id, a_uint32_t port, a_uint32_t phy_id,
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,4,0))
 	struct phy_device *phydev;
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,9,0))
+	struct device_driver *drv;
+	struct phy_driver *phydrv;
+#endif
 	a_uint32_t addr = 0;
 	struct mii_bus *bus;
 
@@ -344,8 +348,15 @@ int sfp_phy_device_setup(a_uint32_t dev_id, a_uint32_t port, a_uint32_t phy_id,
 		return SW_NOT_FOUND;
 #if defined(IN_PHY_I2C_MODE)
 	if (hsl_port_phy_access_type_get(dev_id, port) == PHY_I2C_ACCESS) {
-		if(phydev->drv)
+		if(phydev->drv) {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,9,0))
+			drv = phydev->mdio.dev.driver;
+			phydrv = to_phy_driver(drv);
+			phy_driver_unregister(phydrv);
+#else
 			phy_driver_unregister(phydev->drv);
+#endif
+		}
 	}
 #endif
 #endif

commit 709382c7aefbd07643db24edb514322f078c5181

hsl: phy: sfp: fix unused variables
We need to also check for IN_PHY_I2C_MODE since IPQ50xx does not define it
and thus we will have unused variables.

Fixes: 90cbee1f39d6 ("hsl: phy: sfp: work around constified phydev->drv")
Signed-off-by: Robert Marko <robimarko@gmail.com>

src/hsl/phy/sfp_phy.c
@@ -307,7 +307,7 @@ int sfp_phy_device_setup(a_uint32_t dev_id, a_uint32_t port, a_uint32_t phy_id,
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,4,0))
 	struct phy_device *phydev;
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,9,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,9,0)) && defined(IN_PHY_I2C_MODE)
 	struct device_driver *drv;
 	struct phy_driver *phydrv;
 #endif